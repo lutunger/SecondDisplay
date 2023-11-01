f7f51d3cb0c1e756e1b7f823000ca43d4ab782dd
ji-hwan <lutungerst@gmail.com>
2023-11-02 00:26:40 +0900

    에러 페이지 구현

---
+++ src/main/java/com/project/secondDisplay/common/ExceptionController.java
@@ -0,0 +1,21 @@
+package com.project.secondDisplay.common;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+	
+	@ExceptionHandler(Exception.class)
+	public String exceptionHandler(Exception e, Model model) {
+		
+		
+		e.printStackTrace();
+		
+		model.addAttribute("e", e);
+		
+		return "common/error";
+	}
+	
+}
